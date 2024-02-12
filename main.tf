fbd3b214ca485b6a48e5d6ecf4d9e10f9aa7cb06
john.galeano <john.galeano@gmfinancial.com>
2024-02-12 18:09:58 -0500

    Pre ADF 3

--- main.tf
+++ main.tf
@@ -124,7 +124,6 @@ resource "azurerm_data_factory_linked_service_data_lake_storage_gen2" "example" 
   count                = var.adf_deploy_flag ? 1 : 0
   name                 = "linked_adls"
   data_factory_id      = azurerm_data_factory.adf[0].id
-  service_principal_id = data.azurerm_client_config.current.client_id
   tenant               = var.az_tenant
   url                  = "https://${var.adls_name}dfs.core.windows.net/"
 }
